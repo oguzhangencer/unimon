cd5c890fdca180671f1885ecfc4b2e9271015c24
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-12-06 15:45:40 +0300

    style changes in sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,7 @@
 import { Avatar, Text } from "@mantine/core";
-import { Link, NavLink } from "react-router-dom";
-import { MdOutlineSpaceDashboard, MdSpaceDashboard } from "react-icons/md";
-import { IoSettingsOutline, IoSettingsSharp } from "react-icons/io5";
-import { FiMonitor } from "react-icons/fi";
-import { HiOutlineUsers } from "react-icons/hi";
+import { NavLink } from "react-router-dom";
+import { MdSpaceDashboard } from "react-icons/md";
+import { IoSettingsSharp } from "react-icons/io5";
 import { FaUserFriends } from "react-icons/fa";
 import { GiBoxUnpacking } from "react-icons/gi";
 
@@ -31,11 +29,11 @@ export const Sidebar = () => {
     },
   ];
   return (
-    <div className="flex flex-col items-center justify-start fixed top-0 bottom-0 p-2 w-[300px] overflow-y-auto bg-white rounded-r-3xl">
+    <div className="flex flex-col items-center justify-start fixed top-0 bottom-0 p-2 w-[250px] overflow-y-auto bg-slate-800">
       {/* UniMon Header */}
       <div className="flex p-4">
         {/* TODO: Add Icon or Png for UniMon Logo */}
-        <Text className=" font-semibold text-4xl">UniMon</Text>
+        <Text className=" font-semibold text-4xl text-white">UniMon</Text>
       </div>
       {/* User Avatar & User Name */}
       <div className="flex flex-col items-center p-4 my-6 gap-y-2">
@@ -44,13 +42,13 @@ export const Sidebar = () => {
           size="xl"
           src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=250&q=80"
         />
-        <Text className="font-bold text-xl">Oguz</Text>
+        <Text className="font-bold text-xl text-white">Oguz</Text>
       </div>
       {/* Rotation Links */}
-      <div className="flex flex-col items-start p-4 gap-y-4">
+      <div className="flex flex-col items-stretch gap-y-4">
         {pages.map((page) => (
           <NavLink to={page.path} className="link">
-            <div className="flex gap-x-2 items-center hover:bg-zinc-200 text-2xl text-zinc-900 p-2 rounded-xl">
+            <div className="flex gap-x-2 gap-y-4 p-2 items-center justify-stretch hover:bg-zinc-200 hover:bg-opacity-30 text-2xl text-gray-400 focus:text-white rounded-xl">
               {page.icon}
               {page.title}
             </div>
