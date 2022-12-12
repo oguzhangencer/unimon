b2c459b7b92185d537c959f14406d259bb0f1f56
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-12-12 14:33:51 +0300

    getPackages created

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,9 +1,15 @@
-import { GetPackages } from "./../constans/endpoints";
+import { GetPackages, PutPackages } from "./../constans/endpoints";
 import { GetUsers } from "../constans/endpoints";
 import axios from "./axios";
 
 // Fetching Users
 export const getUsers = async () => {
   const response = await axios.get(GetUsers);
+  return response.data;
+};
+
+// Fetching Packages
+export const getPackages = async () => {
+  const response = await axios.get(PutPackages);
   return response.data;
 };
