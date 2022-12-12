866a1f397419ae2c869a00e05065877eb66975a2
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-12-12 14:49:22 +0300

    created columns

--- src/pages/Packages.tsx
+++ src/pages/Packages.tsx
@@ -1,9 +1,27 @@
+import { Text } from "@mantine/core";
+
 export const Packages = () => {
   return (
     <div className="flex">
+      {/* Package 1 */}
+      <div className="flex flex-col">
+        {/* Header */}
+        <div></div>
+        {/* Feauters */}
+        <div></div>
+        {/* Includes */}
+        <div>
+          <Text>Includes</Text>
+        </div>
+      </div>
+
+      {/* Package 2 */}
       <div className="flex flex-col"></div>
+
+      {/* Package 3 */}
       <div className="flex flex-col"></div>
-      <div className="flex flex-col"></div>
+
+      {/* Package 4 */}
       <div className="flex flex-col"></div>
     </div>
   );
