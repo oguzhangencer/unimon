9299fa9c529faa2a3a05f102c14763318cf6447b
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-12-12 14:17:47 +0300

    adds avatar

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,3 +1,10 @@
+import { Avatar, Text } from "@mantine/core";
+
 export const Settings = () => {
-  return <div>Settings</div>;
+  return (
+    <div className="flex flex-col items-center justify-center p-6">
+      <Text className="text-slate-800">Edit Profile</Text>
+      <Avatar radius="xl" size="xl" />
+    </div>
+  );
 };
