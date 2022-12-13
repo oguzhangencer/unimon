ec3b2e85da16e6ab89a766ff786240e14ae9a4b8
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-12-13 15:27:33 +0300

    packages adds

--- src/pages/Packages.tsx
+++ src/pages/Packages.tsx
@@ -1,5 +1,8 @@
 import { useQuery } from "react-query";
+import { EnterpricePackage } from "../components/packages/EnterprisePackage";
 import { FreePackage } from "../components/packages/FreePackage";
+import { ProPackage } from "../components/packages/ProPackage";
+import { StarterPackage } from "../components/packages/StarterPackage";
 import { getPackages } from "../utils/fetchData";
 
 export const Packages = () => {
@@ -7,9 +10,12 @@ export const Packages = () => {
 
   if (isLoading) return <div>Loading...</div>;
   return (
-    <div className="flex gap-y-6">
+    <div className="flex gap-10 ">
       {/* Packages */}
       <FreePackage />
+      <StarterPackage />
+      <ProPackage />
+      <EnterpricePackage />
     </div>
   );
 };
