a42a5f4c94126bfcdf51bde33410dd56c5634521
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-12-13 11:47:30 +0300

    enterprice package creates

---
+++ src/components/packages/EnterprisePackage.tsx
@@ -0,0 +1,55 @@
+import { useQuery } from "react-query";
+import { getPackages } from "../../utils/fetchData";
+import { Checkbox, Text } from "@mantine/core";
+import { IPackages } from "../../types/interfaces";
+
+export const EnterpricePackage = () => {
+  const { isLoading, isError, data, error } = useQuery("packages", getPackages);
+
+  if (isLoading) return <div>Loading...</div>;
+
+  return (
+    <div>
+      {data
+        ?.filter((item: IPackages) => item?.name === "Tier 3")
+        .map((item: IPackages, index: number) => (
+          <div key={index}>
+            {/* Headers */}
+            <div key={index}>
+              <Text>{item?.name}</Text>
+            </div>
+            {/* Features */}
+            <div>
+              <Text key={index}>{item?.monitors} monitors</Text>
+              <Text key={index}>{item?.users} user</Text>
+              <Text key={index}>{item?.sms} SMS</Text>
+              <Text key={index}>{item?.voiceCall} Voice Call</Text>
+              <Text key={index}>{item?.statusPage} status pages</Text>
+              <Text key={index}>{item?.integrations} integrations</Text>
+              <Text key={index}>{item?.logRetention} years log retention</Text>
+            </div>
+            {/* Includes */}
+            <div>
+              <Checkbox.Group
+                orientation="vertical"
+                label="Includes"
+                withAsterisk
+                spacing="xs"
+                // size="xs"
+              >
+                <Checkbox value={item?.higherChek} label={item?.higherChek} />
+                <Checkbox
+                  value={item?.premiumSupport}
+                  label={item?.premiumSupport}
+                />
+                <Checkbox
+                  value={item?.dedicatedAccount}
+                  label={item?.dedicatedAccount}
+                />
+              </Checkbox.Group>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
