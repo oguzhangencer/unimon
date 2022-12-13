c5c664d96fbcfecc020f9492db58a40a38d21597
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-12-13 11:19:20 +0300

    IPackages interface type

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -25,11 +25,44 @@ export interface IAddress {
 }
 
 export interface IAuth {
+  id: string;
   firstName: string;
   lastName: string;
   email: string;
   phone: string;
   address: IAddress;
   password: string;
+}
+
+export interface IPackages {
   id: string;
+  name: string;
+  monitors: string;
+  users: string;
+  sms: string;
+  voiceCall: string;
+  statusPage: string;
+  integrations: string;
+  logRetention: string;
+  uptime: string;
+  ping: string;
+  tcp: string;
+  dns: string;
+  text: string;
+  screenShot: string;
+  ssl: string;
+  lightHouse: string;
+  domainExpiration: string;
+  advancedRequest: string;
+  customDns: string;
+  sourceCode: string;
+  network: string;
+  whoIs: string;
+  selfHosted: string;
+  cookie: string;
+  ipBlacklist: string;
+  multiLocation: string;
+  higherChek: string;
+  premiumSupport: string;
+  dedicatedAccount: string;
 }
