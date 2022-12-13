b5b47d60b98b53e9ce280cce53e20e637a2dc790
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-12-13 11:20:07 +0300

    getPackage end points changed

--- src/constans/endpoints.ts
+++ src/constans/endpoints.ts
@@ -1,4 +1,4 @@
 export const GetUsers = "/users";
-export const GetPackages = "/packages/";
+export const GetPackages = "/packages";
 export const PutPackages = "/packages/";
 export const PutAuth = `/auth/`;
